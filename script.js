a81f7d75be2d559c560606f34e68eed2a47fc145
GBaldez <83733139+GBaldez@users.noreply.github.com>
2022-04-30 14:06:17 -0300

    feed section

---
+++ script.js
@@ -0,0 +1,7 @@
+let commentFocus = document.querySelector('.comment-input');
+const button = document.querySelector('.publish');
+const changeColorTextButton = () => {
+               button.style.color = "#1e7dfa";
+       }
+
+       
